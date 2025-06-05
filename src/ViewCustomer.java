f38395fe4cf96d454bc73f98cb819cb8d8cb612b
Manukagallage7 <manukakavinda1110@gmail.com>
2025-06-05 09:08:09 +0530

    Add additional fields and back button to ViewCustomer class for enhanced customer information display

--- src/ViewCustomer.java
+++ src/ViewCustomer.java
@@ -4,6 +4,8 @@ import javax.swing.*;
 
 public class ViewCustomer extends JFrame{
 
+    JButton back;
+
     ViewCustomer(){
         setBounds(450, 180, 900, 620);
         getContentPane().setBackground(Color.WHITE);
@@ -14,8 +16,83 @@ public class ViewCustomer extends JFrame{
         add(lblusername);
 
         JLabel labelusername = new JLabel("");
-        labelusername.setBounds(30, 50, 150, 25);
+        labelusername.setBounds(220, 50, 150, 25);
         add(labelusername);
+
+        JLabel lblid = new JLabel("id");
+        lblid.setBounds(30, 110, 150, 25);
+        add(lblid);
+
+        JLabel labelid = new JLabel();
+        labelid.setBounds(220, 50, 150, 25);
+        add(labelid);
+
+        JLabel lblnumber = new JLabel("Number");
+        lblnumber.setBounds(30, 170, 150, 25);
+        add(lblnumber);
+
+        JLabel labelnumber = new JLabel();
+        labelnumber.setBounds(220, 170, 150, 25);
+        add(labelnumber);
+
+        JLabel lblname = new JLabel("Name");
+        lblname.setBounds(30, 230, 150, 25);
+        add(lblname);
+
+        JLabel labelname = new JLabel();
+        labelname.setBounds(220, 230, 150, 25);
+        add(labelname);
+
+        JLabel lblgender = new JLabel("Gender");
+        lblgender.setBounds(30, 290, 150, 25);
+        add(lblgender);
+
+        JLabel labelgender = new JLabel();
+        labelgender.setBounds(220, 290, 150, 25);
+        add(labelgender);
+
+        JLabel lblcountry = new JLabel("Country");
+        lblcountry.setBounds(500, 50, 150, 25);
+        add(lblcountry);
+
+        JLabel labelcountry = new JLabel();
+        labelcountry.setBounds(690, 50, 150, 25);
+        add(labelcountry);
+
+        JLabel lbladdress = new JLabel("Address");
+        lbladdress.setBounds(500, 110, 150, 25);
+        add(lbladdress);
+
+        JLabel labeladdress = new JLabel();
+        labeladdress.setBounds(690, 110, 150, 25);
+        add(labeladdress);
+
+        JLabel lblphone = new JLabel("Phone");
+        lblphone.setBounds(500, 170, 150, 25);
+        add(lblphone);
+
+        JLabel labelphone = new JLabel();
+        labelphone.setBounds(690, 170, 150, 25);
+        add(labelphone);
+
+        JLabel lblemail = new JLabel("Email");
+        lblemail.setBounds(500, 230, 150, 25);
+        add(lblemail);
+
+        JLabel labelemail = new JLabel();
+        labelemail.setBounds(690, 230, 150, 25);
+        add(labelemail);
+
+        back = new JButton("Back");
+        back.setBackground(Color.BLACK);
+        back.setForeground(Color.WHITE);
+        back.setBounds(350, 350, 100, 25);
+        add(back);
+
+
+
+
+
 
         
 
